808c575114ad0acf24840d7993f7448b41999395
ZealousMacaroni <scm6422@proton.me>
2025-10-20 18:20:39 -0500

    Create file reading method

--- include/shader.h
+++ include/shader.h
@@ -5,6 +5,9 @@
 
 #pragma once
 
+#include "string.h"
+#include <fstream>
+
 /**
  * @brief A simple class which handles shader creation and use w/uniforms. 
  *
@@ -13,3 +16,25 @@ class ShaderInstance {
 	ShaderInstance() {}			// Default constructor
 	
 };
+
+/**
+ * @brief A simple function which reads a file.
+ * @param Path The path of the file to read.
+ * @return Returns an std::string which contains the contents of the file.
+ */
+std::string GetContentFromFile(std::string Path) {
+	// Creating file
+	std::ifstream File(Path);
+	
+	// Strings for use later
+	std::string Line, Source;
+	
+	// While there is content to read, run the loop.
+	while(getline(File, Line)) {
+		// Appending the source
+		Source.append(Line + '\n');
+	}
+	
+	// Returning the file contents
+	return Source;
+}
