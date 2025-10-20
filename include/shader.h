78d62268b2c4a8679249e5eac3bdd11fe88a5e3f
ZealousMacaroni <scm6422@proton.me>
2025-10-20 18:12:29 -0500

    Create shader.h and add basic docs/definitions

---
+++ include/shader.h
@@ -0,0 +1,15 @@
+/**
+ * @file shader.h
+ * @brief Contains tools needed for shader creation and use in rendering.
+ */
+
+#pragma once
+
+/**
+ * @brief A simple class which handles shader creation and use w/uniforms. 
+ *
+ */
+class ShaderInstance {
+	ShaderInstance() {}			// Default constructor
+	
+};
