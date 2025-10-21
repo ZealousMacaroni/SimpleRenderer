5f03b87fb0b1e5581ee387dff07bd34790883bda
ZealousMacaroni <scm6422@proton.me>
2025-10-20 19:48:02 -0500

    Add camera.h

---
+++ include/camera.h
@@ -0,0 +1,17 @@
+/**
+ * @file camera.h
+ * @brief Contains everything needed to make a configurable camera.
+ */
+
+#pragma once
+
+/**
+ * @class CameraInstance
+ * @brief Contains methods needed to get the view matrix.
+ */
+class CameraInstance {
+public:
+
+private:
+	
+};
