64c741f943fd48974729205c79ae363a9ce65b72
ZealousMacaroni <scm6422@proton.me>
2025-10-25 14:22:40 -0500

    Add todos

--- include/camera.h
+++ include/camera.h
@@ -8,10 +8,13 @@
 /**
  * @class CameraInstance
  * @brief Contains methods needed to get the view matrix.
+ * @todo Add an input registry and make it so it isn't per class.
+ * @todo Make it conifgureable
+ * @todo Add DeltaTime, i.e a timer
  */
 class CameraInstance {
 public:
-
+	
 private:
 	
 };
