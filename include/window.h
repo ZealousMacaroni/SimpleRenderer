f052ee84893d8314b1e808c0a92bbe2916e70b85
ZealousMacaroni <scm6422@proton.me>
2025-11-01 15:27:04 -0500

    Remove WindowInstanceParameters, instead use the parameters in the constructor

--- include/window.h
+++ include/window.h
@@ -9,30 +9,6 @@
 #include "string"
 
 /**
- * @brief A struct which contains all the data neccessary for window configuration.
- */
-struct WindowInstanceParameters {
-	WindowInstanceParameters() {}	// Default constructor
-									
-	/**
-	 * @brief A constructor which takes in all of the data for the struct.
-	 * @param _Title Title of the window to be displayed.
-	 * @param _Width Width of the window in pixels.
-	 * @param _Height Height of the window in pixels.
-	 * @param _VersionMajor The major version of OpenGL, eg 4 in 4.1.
-	 * @param _VersionMinor The minor version of OpenGL, eg 1 in 4.1.
-	 */
-	WindowInstanceParameters(const char* _Title, int _Width, int _Height, int _VersionMajor, int _VersionMinor) : Title(_Title), Width(_Width), Height(_Height), VersionMajor(_VersionMajor), VersionMinor(_VersionMinor) {}
-	
-	const char* Title;			// String which contains the window title.
-	int Width;					// Int which contains the width of the window in pixels.
-	int Height;					// Int which contains the height of the window in pixels.
-	int VersionMajor;			// The major version of OpenGL, e.g. 1 in v1.2.
-	int VersionMinor;			// The minor version of OpenGL, e.g. 2 in v1.2.
-	
-};
-
-/**
  * @class WindowInstance
  * @brief A class representing a single window.
  */
@@ -44,16 +20,16 @@ public:
 	 * @brief A constructor which creates a window and initializes the GLFW context.
 	 * @param Parameters A WindowInstanceParameters which contains all data for the window.
 	 */
-	WindowInstance(WindowInstanceParameters& Parameters) : Data(Parameters) {
+	WindowInstance(const char* _Title, int _Width, int _Height, int VersionMajor, int VersionMinor) : Title(_Title), Width(_Width), Height(_Height) {
 		// Prepping window creation
 		glfwInit();
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, Data.VersionMajor);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, Data.VersionMinor);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, VersionMajor);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, VersionMinor);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 		
 		// Creating window
-		Window = glfwCreateWindow(Data.Width, Data.Height, Data.Title, NULL, NULL);
+		Window = glfwCreateWindow(Width, Height,Title, NULL, NULL);
 		
 		// Disabling cursor
 		glfwSetInputMode(Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
@@ -142,7 +118,7 @@ public:
 	 * @return Returns an int which is the width of the window.
 	 */
 	int GetWindowWidth() {
-		return Data.Width;
+		return Width;
 	}
 	
 	/**
@@ -150,7 +126,7 @@ public:
 	 * @return Returns an int which is the height of the window.
 	 */
 	int GetWindowHeight() {
-		return Data.Height;
+		return Height;
 	}
 	
 	/**
@@ -178,7 +154,9 @@ public:
 								
 private:
 	GLFWwindow* Window;			// The pointer to the window.
-	WindowInstanceParameters Data;	// The data for this window instance.
+	const char* Title;			// Title of the window to be displayed
+	int Width;					// Width of the window in pixels
+	int Height;					// Height of the window in pixels
 	static int WindowCount;		// The count of windows across all instances of Windows.
 	
 };
