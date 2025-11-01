3f1a36b178225448f8bcc590b58a34b4d7f54136
ZealousMacaroni <scm6422@proton.me>
2025-11-01 17:02:56 -0500

    Create sr.h for easily including all files needed

---
+++ include/SimpleRenderer/sr.h
@@ -0,0 +1,12 @@
+/**
+ * @file sr.h
+ * @brief Includes all headers associated with simple renderer.h.
+ */
+ 
+#pragma once
+ 
+#include <SimpleRenderer/camera.h>
+#include <SimpleRenderer/object.h>
+#include <SimpleRenderer/renderer.h>
+#include <SimpleRenderer/shader.h>
+#include <SimpleRenderer/window.h>
